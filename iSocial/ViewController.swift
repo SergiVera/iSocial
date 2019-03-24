cfa557485631f42d4e02ee1db2a11da8b510a741
Sergi Vera Martínez <sergitus1998@gmail.com>
2019-03-24 18:30:04 +0100

    Initial Commit

---
+++ iSocial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iSocial
+//
+//  Created by Sergi Vera Martínez on 24/03/2019.
+//  Copyright © 2019 Sergi Vera Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
